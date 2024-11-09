75eec7abc3a836027992449352ed2f54d4863379
Andrew Kiema <andrewkiema94@gmail.com>
2024-11-09 12:47:46 +0300

    Printing table content

--- SeleniumWebDriver/src/test/java/day12/Staticwebtables.java
+++ SeleniumWebDriver/src/test/java/day12/Staticwebtables.java
@@ -23,17 +23,17 @@ public class Staticwebtables {
         	System.out.println(col.getText());
         }*/
         WebElement focus=chd.findElement(By.xpath("//table[@name='BookTable']/tbody/tr[6]/td[1]"));
-        System.out.println(focus.getText());
+        //System.out.println(focus.getText());
         /*for(int k=0; k<=columnno.size(); k++) {
         	System.out.println(columnno.get(k).getText());
-        }
-        /*for(int i=2; i<=rowsnumber.size();i++){
-        
-        		String items=chd.findElement(By.xpath("//table[@name='BookTable']/tbody/tr["+i+"]/td[2]")).getText();
-        		System.out.printf("%-20s", items);
-        	
-        	System.out.println();
         }*/
+        for(int i=2; i<rowsnumber.size();i++){
+          for(int j=1; j<=columnno.size();j++) {
+        	String items=chd.findElement(By.xpath("//table[@name='BookTable']/tbody/tr["+i+"]/td["+j+"]")).getText();
+      		System.out.printf("%-20s", items);
+          }  
+          System.out.println();
+        }
         //%: Begins a format specifier.
         //-20s: Left-aligns the text within a field width of 20 characters. Adjust 20 to fit your data better if needed.
 	}
--- SeleniumWebDriver/src/test/java/day13/Paginationattempt.java
+++ SeleniumWebDriver/src/test/java/day13/Paginationattempt.java
@@ -1,8 +1,10 @@
 package day13;
 
 import java.time.Duration;
+import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Paginationattempt {
@@ -12,10 +14,18 @@ public class Paginationattempt {
         ChromeDriver c= new ChromeDriver();
         c.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         c.get("https://testautomationpractice.blogspot.com/");
+        List<WebElement>buttons=c.findElements(By.xpath("//ul[@id='pagination']/li"));
         c.manage().window().maximize();
-        for(int i=2; i<=4; i++) {
-        	if(i>1) {
+        for(int i=1; i<=2; i++) {
+        	if(i>=1) {
         		c.findElement(By.xpath("//ul[@id='pagination']/li/a[text()="+i+"]")).click();
+        		for(int r=1; r<=5; r++) {
+        			for(int col=1; col<=4; col++) {
+        				String info=c.findElement(By.xpath("//table[@id='productTable']/tbody/tr["+r+"]/td["+col+"]")).getText();
+        				System.out.printf("%-20s",info);
+        			}
+        			System.out.println();
+        		}
         		Thread.sleep(3000);
         	}
         }
