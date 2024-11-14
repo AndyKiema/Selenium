94ae958ce927c5e4c4682b30a23143df97f27625
Andrew Kiema <andrewkiema94@gmail.com>
2024-11-14 20:13:41 +0300

    SSL certificate handling

---
+++ SeleniumWebDriver/src/test/java/day18/SSLhandling.java
@@ -0,0 +1,22 @@
+package day18;
+
+import java.time.Duration;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SSLhandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeOptions options= new ChromeOptions();
+		options.setAcceptInsecureCerts(true); // Accepts SSL certificates
+		
+        ChromeDriver chd= new ChromeDriver();
+        chd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        chd.get("https://expired.badssl.com/");
+        chd.manage().window().maximize();
+        System.out.println(chd.getTitle());
+	}
+
+}
