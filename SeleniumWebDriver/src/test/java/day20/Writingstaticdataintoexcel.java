d70e65aa1814678d24cfce72029aa8ee27cc7f29
Andrew Kiema <andrewkiema94@gmail.com>
2024-11-17 23:09:03 +0300

    Writing static data into Excel

---
+++ SeleniumWebDriver/src/test/java/day20/Writingstaticdataintoexcel.java
@@ -0,0 +1,40 @@
+package day20;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Writingstaticdataintoexcel {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+       // Creating new excel file
+	   FileOutputStream file= new FileOutputStream(System.getProperty("user.dir")+"\\Testdatafolder\\Datatoinput.xlsx"); //Datatoinput is the name of the file
+	   XSSFWorkbook workbook= new XSSFWorkbook();
+	   XSSFSheet sheet= workbook.createSheet("Info"); //Info is the name of the sheet
+	   //Creating rows and their cells
+	   XSSFRow firstrow=sheet.createRow(0);
+	   firstrow.createCell(0).setCellValue("Welcome");
+	   firstrow.createCell(1).setCellValue("to");
+	   firstrow.createCell(2).setCellValue("Selenium");
+	   
+	   XSSFRow secondrow=sheet.createRow(1);
+	   secondrow.createCell(0).setCellValue("Automation");
+	   secondrow.createCell(1).setCellValue("Testing");
+	   secondrow.createCell(2).setCellValue("Lessons");
+	   
+	   XSSFRow thirdrow=sheet.createRow(2);
+	   thirdrow.createCell(0).setCellValue("My");
+	   thirdrow.createCell(1).setCellValue("Name is");
+	   thirdrow.createCell(2).setCellValue("Andrew");
+	   
+	   workbook.write(file);
+	   workbook.close();
+	   file.close();
+	   System.out.println("File created");
+	}
+
+}
